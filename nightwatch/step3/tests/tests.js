e95f44c05b2ab31f0239cec5ccee21ba457d2fe3
Martinez <31023666+time2fishman@users.noreply.github.com>
2018-01-22 10:07:00 -0700

    3.1 stretch

--- nightwatch/step3/tests/tests.js
+++ nightwatch/step3/tests/tests.js
@@ -1,13 +1,15 @@
 const selectors = require('../supporting/selectors')
+const functions = require('../supporting/functions')
 
 module.exports = {
-    beforeEach : browser => {
+    beforeEach: browser => {
         browser.url('http://localhost:3000')
     },
-    after : browser => {
+    after: browser => {
         browser.end()
     },
-    '2+2=4' : browser => {
+    'UI Check': browser => functions.uiChecker(browser),
+    '2+2=4': browser => {
         //I click all the appropriate buttons and check the display for the appropriate results, per the steps of my test case
         browser
             .click(selectors['2'])
@@ -21,5 +23,15 @@ module.exports = {
         browser
             .click(selectors['='])
             .expect.element(selectors['result']).text.to.equal('4')
+    },
+    '32.1*2=64.2': browser => {
+        functions.buttonClicker(browser, '3')
+        functions.buttonClicker(browser, '2')
+        functions.buttonClicker(browser, '.')
+        functions.buttonClicker(browser, '1')
+        functions.buttonClicker(browser, '*')
+        functions.buttonClicker(browser, '2')
+        functions.buttonClicker(browser, '=')
+        browser.expect.element(selectors['result']).text.to.equal('64.2')
     }
 }
