cb1a5564784dddd257615201266163ad97a079b9
Martinez <31023666+time2fishman@users.noreply.github.com>
2018-01-22 09:30:21 -0700

    QA3 2.5 assessment

--- nightwatch/step1/tests/tests.js
+++ nightwatch/step1/tests/tests.js
@@ -1,8 +1,26 @@
+const selectors = require('../../test_data/selectors')
+
 module.exports = {
-    beforeEach : browser => {
+    beforeEach: browser => {
         browser.url('http://localhost:3000')
     },
-    after : browser => {
+
+    after: browser => {
         browser.end()
+    },
+
+    '3*2=6': browser => {
+        browser
+            .click(selectors.numButtons.three)
+            .expect.element(selectors.display.result).text.to.equal('3')
+        browser
+            .click(selectors.cmdButtons.multiply)
+            .expect.element(selectors.display.result).text.to.equal('0')
+        browser
+            .click(selectors.numButtons.two)
+            .expect.element(selectors.display.result).text.to.equal('2')
+        browser
+            .click(selectors.cmdButtons.equals)
+            .expect.element(selectors.display.result).text.to.equal('6')
     }
 }
---
+++ nightwatch/test_data/selectors.js
@@ -0,0 +1,31 @@
+module.exports = {
+
+    cmdButtons: {
+        clear: 'button[name="clearButton"]',
+        posNeg: 'button[name="negativeButton"]',
+        percent: 'button[name="percenetButton"]',
+        divide: 'button[name="divideButton"]',
+        multiply: 'button[name="multiplyButton"]',
+        subtract: 'button[name="subtractButton"]',
+        add: 'button[name="addButton"]',
+        equals: 'button[name="equalsButton"]',
+    },
+
+    display: {
+        result: 'span[name="result"]',
+    },
+
+    numButtons: {
+        one: 'button[name="1Button"]',
+        two: 'button[name="2Button"]',
+        three: 'button[name="3Button"]',
+        four: 'button[name="4Button"]',
+        five: 'button[name="5Button"]',
+        six: 'button[name="6Button"]',
+        seven: 'button[name="7Button"]',
+        eight: 'button[name="8Button"]',
+        nine: 'button[name="9Button"]',
+        zero: 'button[name="0Button"]',
+        decimal: 'button[name="decimalButton"]',
+    }
+}
